6922d6fc03a69b556c2d1c9154f0baed3534ee8b
Denis Kiryanov <denis.kiryanov@arcadia.spb.ru>
2020-03-05 00:43:27 +0300

    TurboFeedItemTests added.

---
+++ Tests.Unit/Feed/TurboFeedItemTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using YandexTurboRss.Constants;
+using YandexTurboRss.Feed;
+
+namespace Tests.Unit.Feed
+{
+    [TestFixture()]
+    public class TurboFeedItemTests
+    {
+        private readonly XNamespace _turboYandexNamespace = Namespaces.TurboYandex;
+
+        [Test]
+        public void ToXElement_ReturnsCorrectXElement()
+        {
+            // Arrange
+            DateTime pubDate = DateTime.Now;
+            TurboFeedItem item = new TurboFeedItem()
+            {
+                Link = "https://sample.ru",
+                Author = "Author 1",
+                Content = "Content 1",
+                PubDate = pubDate,
+                Source = "https://source2.ru",
+                Topic = "Item 1",
+                Turbo = "true"
+            };
+
+            // Act
+            XElement result = item.ToXElement();
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().HaveElement("link", item.Link);
+            result.Should().HaveElement(_turboYandexNamespace + "source", item.Source);
+            result.Should().HaveElement(_turboYandexNamespace + "topic").Which.Value.Should().BeEquivalentTo("Item 1");
+            result.Should().HaveElement("pubDate", item.PubDate.ToString("R"));
+            result.Should().HaveElement("author", item.Author);
+            result.Should().HaveElement(_turboYandexNamespace + "content", item.Content);
+            result.Should().HaveAttribute("turbo", item.Turbo);
+        }
+
+        [Test]
+        public void TurboFeedItem_HasDefaultTurboPropertySetToTrue()
+        {
+            // Act
+            TurboFeedItem result = new TurboFeedItem();
+
+            // Assert
+            result.Turbo.Should().BeEquivalentTo("true");
+        }
+    }
+}
