d43170e3bb157500b2c5eebc44b7b9ce9eccea03
Denis Kiryanov <denis.kiryanov@arcadia.spb.ru>
2020-03-10 00:48:33 +0300

    Nuget package was added.

--- YandexTurboRss/Properties/AssemblyInfo.cs
+++ YandexTurboRss/Properties/AssemblyInfo.cs
@@ -1,13 +1,14 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("YandexTurboRss")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Yandex turbo pages RSS generator")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Denis Kiryanov")]
 [assembly: AssemblyProduct("YandexTurboRss")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
@@ -33,3 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("")]
+
